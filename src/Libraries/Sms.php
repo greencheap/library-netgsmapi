00526795baeff4befe63d9c2c835422651d55190
Yunus SENEM <me@senem.co>
2019-12-06 11:21:45 +0300

    Update Sms.php

--- src/Libraries/Sms.php
+++ src/Libraries/Sms.php
@@ -102,7 +102,7 @@ class Sms
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,"{$this->conf['api']}sms/blacklist/xml");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,2);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml"));
